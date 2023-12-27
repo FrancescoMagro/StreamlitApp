e844f05e8b9d94038ab5aa5e0b422b48f8d5dadf
FrancescoMagro <fmagro7@gmail.com>
2023-12-27 18:38:56 +0100

    commit

---
+++ code/Home_Screen.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home Screen",
+    page_icon="👋",
+)
+
+st.write("# Welcome To Our Database Analysis Project 👋")
+
+st.sidebar.success("Select an Analysis above.")
+
