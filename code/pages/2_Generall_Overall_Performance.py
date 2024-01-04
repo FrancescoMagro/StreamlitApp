60b8f82eaa42c43b14644b0a86e4a04ca2cd52db
FrancescoMagro <fmagro7@gmail.com>
2024-01-04 23:51:45 +0100

    Update Map

--- code/pages/2_Generall_Overall_Performance.py
+++ code/pages/2_Generall_Overall_Performance.py
@@ -15,40 +15,37 @@ def random_color_generator():
     g = random.randint(0, 255)
     b = random.randint(0, 255)
     return (r, g, b)
-data = {
-    "year": [2023, 2023, 2023, 2023, 2023, 2023, 2023, 2023],
-    "origin": ["LAX", "LAS", "LGA", "BOS", "JFK", "DEN", "DEN", "ATL"],
-    "destination": ["SFO", "LAX", "ORD", "DCA", "LAX", "LAS", "PHX", "MCO"],
-    "total": [17721, 15952, 15231, 15171, 15032, 14058, 13805, 13319],
-    "origin_lat": [33.9382, 36.0719, 40.7794, 42.3606, 40.6392, 39.8466, 39.8466, 33.6301],
-    "origin_lng": [-118.3865, -115.1634, -73.8803, -71.0097, -73.7639, -104.6562, -104.6562, -84.4418],
-    "destination_lat": [37.6196, 33.9382, 41.9875, 38.8472, 33.9382, 36.0719, 33.4277, 28.4183],
-    "destination_lng": [-122.3656, -118.3865, -87.9319, -77.0345, -118.3865, -115.1634, -112.0038, -81.3241]
-}
+try:
+    df = pd.read_csv('/Users/francescomagro/Desktop/Streamlit/StreamlitApp/code/pages/csv/most_frequent_airports.csv')
+except:
+    df = pd.read_csv('code/pages/csv/most_frequent_airports.csv')
+sorted_years = sorted(df['year'].unique())
+print(sorted_years)
 
-df = pd.DataFrame(data)
+years=st.selectbox(label="Select a year", options=sorted_years)
+print(years)
+filter_df = df[df['year'] == years]
+print(filter_df)
 
-# Fixed set of colors for the routes
 colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF', '#FF00FF', '#800000', '#008000', '#000080', '#808000']
 
-@st.cache(allow_output_mutation=True)
-def create_map():
-    m = folium.Map(location=[df["origin_lat"].mean(), df["origin_lng"].mean()], zoom_start=4)
 
-    for index, row in df.iterrows():
-        color = colors[index % len(colors)]  # Use a fixed color from the list
 
-        origin = [row["origin_lat"], row["origin_lng"]]
-        destination = [row["destination_lat"], row["destination_lng"]]
-        route_name = f"{row['origin']} to {row['destination']}"
+m = folium.Map(location=[filter_df["origin_lat"].mean(), filter_df["origin_lng"].mean()], zoom_start=4)
 
-        folium.PolyLine([origin, destination], color=color, weight=2.5, opacity=1, popup=route_name).add_to(m)
-        folium.CircleMarker(location=origin, radius=5, color=color, fill=True, fill_color=color, popup=f"Origin: {row['origin']}").add_to(m)
-        folium.CircleMarker(location=destination, radius=5, color=color, fill=True, fill_color=color, popup=f"Destination: {row['destination']}").add_to(m)
+for index, row in filter_df.iterrows():
+    color = colors[index % len(colors)]
 
-    return m
+    origin = [row["origin_lat"], row["origin_lng"]]
+    destination = [row["destination_lat"], row["destination_lng"]]
+    route_name = f"{row['origin']} to {row['destination']} Amount of times: {row['total']}"
+
+    folium.PolyLine([origin, destination], color=color, weight=4, opacity=1, popup=route_name).add_to(m)
+    folium.CircleMarker(location=origin, radius=5, color=color, fill=True, fill_color=color, popup=f"Origin: {row['origin']}").add_to(m)
+    folium.CircleMarker(location=destination, radius=5, color=color, fill=True, fill_color=color, popup=f"Destination: {row['destination']}").add_to(m)
 
 # Streamlit app
-st.title('Flight Routes Map')
-map_object = create_map()
-st_folium(map_object, height=700, width=800)
+st.title(f'Flight Routes Map {years}')
+
+st.dataframe(filter_df)
+st_folium(m, height=500, width=900)
